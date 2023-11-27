e5dd550c718a6aa99391ea9245fc27bb87967a19
unknown <softtech.series@gmail.com>
2023-11-27 00:54:05 -0800

    week 1 project

---
+++ week 1/main.py
@@ -0,0 +1,50 @@
+# this is to add two numbers 
+"""
+for multi line comments
+print(help(str,int...))
+
+"""
+x=12
+y=33
+j=x+y
+print(j)
+name="roba"
+print(name)
+y,d,e=44,55,88
+print(d)
+f=d=s=33
+print(s)
+fname="roba"
+lname="rikita"
+print(fname+" "+lname)
+x=12
+r=34.4
+e="roba"
+j=True
+print(type(s))
+m=35
+a=float(m)
+z=int(a)
+c=complex(z)
+print(c) 
+e="hello!12"
+print(e)
+w='hello!12"'
+print(w)
+q="this is roba's book \"intro to eth\""
+print(q)
+r="""jjjjjjjjjjjjjjjjjj
+jjjjjjjjjjjj
+kkkkkkkkkkkkkkkkkk
+kkkkkkkkkkkkkkk
+lllllllllllllllllll
+"""
+print(r)
+u="yoseph"
+
+print(u[2:-1])
+i="yoseph"
+print( i.find('e'))
+print(i.upper())
+u="your name is {} and your age is {}"
+print(u.format('meti', 20))
